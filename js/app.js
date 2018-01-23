d1b1e65d84e2b8a70156f9bce6c727710e2b8460
GirlLollipop <girl.lollipop@gmail.com>
2018-01-23 09:02:19 -0600

    vit

--- js/app.js
+++ js/app.js
@@ -1,19 +1,32 @@
+/*función desaparecer contenedor*/
+$(document).ready(function() {
+    setTimeout(function() {
+        $(".view-splash").fadeOut(1500);
+    },3000);
+});
+
+/*función aparecer contenedor*/
+$(document).ready(function() {
+    setTimeout(function() {
+        $(".home").fadeIn(1700);
+    },4500);
+});
+
 /*select*/
 $(document).ready(function() {
     $('select').material_select();
   });
 
 
-var $phone = $("#phone-input");
+/*var $phone = $("#phone-input");
 var $next = $("#next")
 
-function validarPhone() { /*Función que valina telefono*/
-  var $next = $("#next");/*Botón que agrega contacto*/
-  if($(phone).val().trim().length == 10) {/*Si el valor de la entrada (sin espacios) es mayor a cero*/
-    $next.removeAttr("disabled");/*Se quita el atributo "disabled"*/
+function validarPhone() { Función que valina telefono
+  var $next = $("#next");Botón que agrega contacto
+  if($(phone).val().trim().length == 10) { Si el valor de la entrada (sin espacios) es mayor a cero
+    $next.removeAttr("disabled"); Se quita el atributo "disabled"
   } else {
-    $next.attr("disabled" , true);/*Si no hay valor en la entrada agrega el atributo disabled y le coloca tru (desabilitado)*/
+    $next.attr("disabled" , true); Si no hay valor en la entrada agrega el atributo disabled y le coloca tru (desabilitado)
   }
-
-
-console.log($phone)
+});
+*/
